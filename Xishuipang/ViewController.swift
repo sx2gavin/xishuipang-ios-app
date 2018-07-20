1fffb96e3b5dcf36d649bddf53b08b87422738a3
Gavin <luoxiangyu528@gmail.com>
2018-07-20 11:17:19 -0400

    Initial Commit

---
+++ Xishuipang/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Xishuipang
+//
+//  Created by Xiangyu Luo on 2018-07-20.
+//  Copyright © 2018 Chinese Gospel Church. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
