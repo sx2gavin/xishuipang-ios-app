021d047fb8cbf19a6455e77b8448f1e7e23469a7
Gavin <luoxiangyu528@gmail.com>
2018-12-02 17:37:55 -0500

    Add home page.

--- Xishuipang/HomePageViewController.swift
+++ Xishuipang/HomePageViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomePageViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +19,4 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
---
+++ Xishuipang/VolumeScrollView.swift
@@ -0,0 +1,32 @@
+//
+//  VolumeScrollView.swift
+//  Xishuipang
+//
+//  Created by Xiangyu Luo on 2018-12-02.
+//  Copyright © 2018 Chinese Gospel Church. All rights reserved.
+//
+
+import UIKit
+
+class VolumeScrollView: UIScrollView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder : NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    func loadContentToScrollView() {
+        
+    }
+}
